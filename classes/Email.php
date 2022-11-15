40e970f0396c3452b340ec68c533786429f7b8e0
enzodangelo <enzodan80@gmail.com>
2022-11-14 22:18:24 -0300

    mailtrap

--- classes/Email.php
+++ classes/Email.php
@@ -38,7 +38,7 @@ class Email {
         
         $contenido = '<html>';
         $contenido .= "<p><strong>Hola " . $this->email .  "</strong> Has Creado tu cuenta en App Salón, solo debes confirmarla presionando el siguiente enlace</p>";
-        $contenido .= "<p>Presiona aquí: <a href='http://localhost:3000/confirmar-cuenta?token=" . $this->token . "'>Confirmar Cuenta</a>";        
+        $contenido .= "<p>Presiona aquí: <a href='https://shrouded-springs-10174.herokuapp.com//confirmar-cuenta?token=" . $this->token . "'>Confirmar Cuenta</a>";        
         $contenido .= "<p>Si tu no solicitaste esta cuenta, puedes ignorar el mensaje</p>";
         $contenido .= '</html>';
         $mail->Body = $contenido;
